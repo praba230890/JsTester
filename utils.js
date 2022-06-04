a199bbc4650f99491caf1515d455408117fa2c8d
Prabakaran Santhanam <praba230890@gmail.com>
2022-06-04 09:15:42 +0530

    remove fork

--- utils.js
+++ utils.js
@@ -1,18 +1,4 @@
-import { fork, spawn } from 'child_process';
-
-export function execute_py(cmd, args, options) {
-  return new Promise((resolve, reject) => {
-    const child = fork(cmd);
-    child.on('error', reject);
-    child.on('exit', (code) => {
-      if (code !== 0) {
-        reject(new Error(`Command ${cmd} exited with code ${code}`));
-      } else {
-        resolve();
-      }
-    });
-  });
-}
+import { spawn } from 'child_process';
 
 export function execute_js(cmd) {
     return new Promise((resolve, reject) => {
